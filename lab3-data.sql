a51dabeea7d388c24bbe0ed3cee9b623916ad6f4
kosenkomichael <kosenko_michael_samara@mail.com>
2025-10-25 22:11:10 +0400

    lab_5

--- lab3-data.sql
+++ lab3-data.sql
@@ -24,24 +24,25 @@ INSERT INTO bicycles (serial_number, model_id) VALUES
 ('MTB0052024', 1);
 
 INSERT INTO rental (bicycle_id, customer_id, pick_point_id, return_point_id, start_time, end_time) VALUES 
-(1, 1, 1, 1, '2024-01-15 10:00:00', '2024-01-15 12:30:00'),
-(2, 2, 2, 3, '2024-01-15 11:00:00', '2024-01-15 14:45:00'),
+(1, 1, 1, 1, '2024-01-15 10:00:00', '2024-01-15 14:30:00'),
+(2, 2, 2, 3, '2024-01-15 11:00:00', '2024-01-15 15:30:00'),
+(2, 2, 2, 3, '2024-01-15 5:00:00', '2024-01-15 11:40:00'),
 (3, 3, 1, NULL, '2024-01-16 09:30:00', NULL);
 
-UPDATE models SET model_title = 'Горный велосипед PRO' WHERE model_id = 1;
+#UPDATE models SET model_title = 'Горный велосипед PRO' WHERE model_id = 1;
 
-UPDATE customers SET phone = '+79161112233' WHERE customer_id = 1;
+#UPDATE customers SET phone = '+79161112233' WHERE customer_id = 1;
 
-UPDATE rental_points SET lat = 55.750000, lon = 37.620000 WHERE point_id = 1;
+#UPDATE rental_points SET lat = 55.750000, lon = 37.620000 WHERE point_id = 1;
 
-UPDATE rental SET end_time = '2024-01-16 11:00:00', return_point_id = 2 WHERE rental_id = 3;
+#UPDATE rental SET end_time = '2024-01-16 11:00:00', return_point_id = 2 WHERE rental_id = 3;
 
-DELETE FROM rental WHERE rental_id = 3;
+#DELETE FROM rental WHERE rental_id = 3;
 
-DELETE FROM bicycles WHERE bicycle_id = 5;
+#DELETE FROM bicycles WHERE bicycle_id = 5;
 
-DELETE FROM customers WHERE customer_id = 3;
+#DELETE FROM customers WHERE customer_id = 3;
 
-DELETE FROM rental WHERE rental_id > 0;
+#DELETE FROM rental WHERE rental_id > 0;
 
-DELETE FROM bicycles WHERE bicycle_id > 0;
+#DELETE FROM bicycles WHERE bicycle_id > 0;
